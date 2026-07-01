df380572848e8965fba8a73b9432bbcde8aba3db
Victor <victoraugustoguarieiro@gmail.com>
2026-07-01 14:29:08 -0300

    feat: add Supabase booking integration

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.CICOTE_CONFIG = {
+  supabase: {
+    url: "https://zojiohyiqlkipsveflul.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvamlvaHlpcWxraXBzdmVmbHVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5MTQ1NTAsImV4cCI6MjA5ODQ5MDU1MH0.kYPIVg0jHEzl2QF7LOSpftTZyRf08ZbEwOEa3PwzrdM"
+  }
+};
